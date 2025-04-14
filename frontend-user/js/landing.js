ffb52f81569116d8817e79c55b38d75eff63aa5c
sistamarien2004 <sistamarien2004@gmail.com>
2025-04-14 22:10:31 +0700

    push pertama frontend-user

---
+++ frontend-user/js/landing.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#loginBtn').on('click', function () {
+      window.location.href = '../html/login.html'; // ganti sesuai path
+    });
+  
+    $('#signupBtn').on('click', function () {
+      window.location.href = '../html/register.html'; // ganti sesuai path
+    });
+  });
+  
