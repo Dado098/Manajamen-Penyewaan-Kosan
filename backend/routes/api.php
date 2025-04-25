36aff20916430f1156e46ca21fa8fe9666b6f203
Muhammad Dhapa <mdhapadoang@gmail.com>
2025-04-26 05:54:55 +0700

    Update Codingan

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -30,6 +30,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //  Untuk CRUD Kamar
 Route::apiResource('kamars', KamarController::class);
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
